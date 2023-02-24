a7a0411cf66edd9ae981dc4eecc7c129751c2457
Kevin Amoa <kevin.amoa@space-bx.co.uk>
2023-02-24 00:46:01 +0000

    Cleaned up config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,11 +2,12 @@ module.exports = {
     siteMetadata: {
         title: `kamstech`,
         siteUrl: `https://www.yourdomain.tld`,
-        description: `A Gatsby site`,
-        plugins: [
-            "gatsby-plugin-image",
-            "gatsby-plugin-sharp",
-            "gatsby-transformer-sharp",
-            "gatsby-plugin-netlify"]
-    }
+        description: `A Gatsby site`
+    },
+    plugins: [
+        "gatsby-plugin-image",
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        "gatsby-plugin-netlify"
+    ]
 }
